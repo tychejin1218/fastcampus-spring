179da3402c3429d0f661233a49ac709731b0bdf2
A-Bong <tychejin@naver.com>
2022-04-12 17:47:21 +0900

    Feat: Spring Framework - H2 DB 설명, 간단한 DB 저장 기능 구현

---
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
@@ -0,0 +1,29 @@
+package com.fastcampus.programming.dmaker.service;
+
+import com.fastcampus.programming.dmaker.entity.Developer;
+import com.fastcampus.programming.dmaker.repository.DeveloperRepository;
+import com.fastcampus.programming.dmaker.type.DeveloperLevel;
+import com.fastcampus.programming.dmaker.type.DeveloperSkillType;
+import javax.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DMakerService {
+
+  private final DeveloperRepository developerRepository;
+
+  @Transactional
+  public void createDeveloper() {
+    Developer developer = Developer.builder()
+        .developerLevel(DeveloperLevel.JUNGNIOR)
+        .developerSkillType(DeveloperSkillType.FRONT_END)
+        .experienceYears(2)
+        .name("Olaf")
+        .age(5)
+        .build();
+
+    developerRepository.save(developer);
+  }
+}
---
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/type/DeveloperLevel.java
@@ -0,0 +1,17 @@
+package com.fastcampus.programming.dmaker.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum DeveloperLevel {
+
+  NEW("신입 개발자"),
+  JUNIOR("주니어 개발자"),
+  JUNGNIOR("중간 개발자"),
+  SENIOR("시니어 개발자");
+
+  private final String description;
+}
+  
