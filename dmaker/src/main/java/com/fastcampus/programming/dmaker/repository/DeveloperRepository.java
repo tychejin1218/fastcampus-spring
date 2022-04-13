a1cfa70e761c37996c31100a253072e6c07b6383
A-Bong <tychejin@naver.com>
2022-04-13 15:40:44 +0900

    Feat: Spring Framework - 비즈니스 밸리데이션 적용

--- dmaker/src/main/java/com/fastcampus/programming/dmaker/repository/DeveloperRepository.java
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/repository/DeveloperRepository.java
@@ -1,10 +1,12 @@
 package com.fastcampus.programming.dmaker.repository;
 
 import com.fastcampus.programming.dmaker.entity.Developer;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
 
+  Optional<Developer> findByMemberId(String memberId);
 }
--- dmaker/src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
@@ -1,7 +1,11 @@
 package com.fastcampus.programming.dmaker.service;
 
+import static com.fastcampus.programming.dmaker.exception.DMakerErrorCode.DUPLICATED_MEMBER_ID;
+import static com.fastcampus.programming.dmaker.exception.DMakerErrorCode.LEVEL_EXPERIENCE_YEARS_NOT_MATCHED;
+
 import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programming.dmaker.entity.Developer;
+import com.fastcampus.programming.dmaker.exception.DMakerException;
 import com.fastcampus.programming.dmaker.repository.DeveloperRepository;
 import com.fastcampus.programming.dmaker.type.DeveloperLevel;
 import com.fastcampus.programming.dmaker.type.DeveloperSkillType;
@@ -27,5 +31,29 @@ public class DMakerService {
         .build();
 
     developerRepository.save(developer);
+
+  }
+
+  private void validateCreateDeveloperRequest(CreateDeveloper.Request request) {
+
+    DeveloperLevel developerLevel = request.getDeveloperLevel();
+    Integer experienceYears = request.getExperienceYears();
+    if (developerLevel == DeveloperLevel.SENIOR
+        && experienceYears < 10) {
+      throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+    }
+    if (developerLevel == DeveloperLevel.JUNGNIOR
+        && experienceYears < 4 || experienceYears > 10) {
+      throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+    }
+    if (developerLevel == DeveloperLevel.JUNGNIOR
+        && experienceYears > 4) {
+      throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+    }
+
+    developerRepository.findByMemberId(request.getMemberId())
+        .ifPresent((developer -> {
+          throw new DMakerException(DUPLICATED_MEMBER_ID);
+        }));
   }
 }
