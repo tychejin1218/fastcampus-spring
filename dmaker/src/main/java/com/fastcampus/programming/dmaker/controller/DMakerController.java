beab23487570d054b44d8c4cbcf6f8ae8fa6ceca
A-Bong <tychejin@naver.com>
2022-04-13 15:18:52 +0900

    Feat: Spring Framework - 데이터 밸리데이션 적용

--- dmaker/src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -1,12 +1,16 @@
 package com.fastcampus.programming.dmaker.controller;
 
+import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programming.dmaker.service.DMakerService;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -23,11 +27,12 @@ public class DMakerController {
     return Arrays.asList("snow", "elsa", "Olaf");
   }
 
-  @GetMapping("/create-developer")
-  public List<String> createDevelopers() {
-    log.info("GET /create-developers HTTP/1.1");
+  @PostMapping("/create-developer")
+  public List<String> createDevelopers(
+      @Valid @RequestBody CreateDeveloper.Request request) {
+    log.info("request : {}", request);
 
-    dMakerService.createDeveloper();
+    dMakerService.createDeveloper(request);
 
     return Collections.singletonList("Olaf");
   }
--- dmaker/src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
@@ -1,5 +1,6 @@
 package com.fastcampus.programming.dmaker.service;
 
+import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programming.dmaker.entity.Developer;
 import com.fastcampus.programming.dmaker.repository.DeveloperRepository;
 import com.fastcampus.programming.dmaker.type.DeveloperLevel;
@@ -15,7 +16,8 @@ public class DMakerService {
   private final DeveloperRepository developerRepository;
 
   @Transactional
-  public void createDeveloper() {
+  public void createDeveloper(
+      CreateDeveloper.Request request) {
     Developer developer = Developer.builder()
         .developerLevel(DeveloperLevel.JUNGNIOR)
         .developerSkillType(DeveloperSkillType.FRONT_END)
